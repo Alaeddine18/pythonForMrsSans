39cd7f5ebc892531ba6a7f63ea9957c34ed59c47
Alaeddine18 <adhuilaa@gmail.com>
2023-03-07 13:01:35 +0100

    exo 4

--- isfloatNul.py
+++ isfloatNul.py
@@ -1,4 +1,17 @@
-#Saisissez un flottant S’il est positif ou nul, affichez sa racine, sinon affichez un message d’erreur
+#Saisissez un flottant
+# S’il est positif ou nul, affichez sa racine, sinon affichez un message d’erreur
+import math
+
+x = float(input("Saisissez un flottant : "))
+
+if x >= 0:
+    racine = math.sqrt(x)
+    print("La racine de", x, "est", racine)
+else:
+    print("Erreur : le nombre saisi est négatif.")
+
+
+
 
 
 
